ef658f47fb1afa003b33411291d64b17ef1ece62
Totti00 <gek5800@gmail.com>
2023-03-24 18:17:31 +0100

    finish constant method

--- src/test/scala/u03/StreamsTest.scala
+++ src/test/scala/u03/StreamsTest.scala
@@ -3,13 +3,14 @@ package u03
 import Streams.*
 import org.junit.*
 import org.junit.Assert.*
-import Lists.*
+import Lists.List.*
 
 class StreamsTest {
-  import List.*
 
   @Test def testDrop() =
     val s = Stream.take(Stream.iterate(0)(_ + 1))(10)
     assertEquals(Cons(6, Cons(7, Cons(8, Cons(9, Nil())))), Stream.toList(Stream.drop(s)(6)))
 
+  @Test def testConstant() =
+    assertEquals(Cons("x", Cons("x", Cons("x", Cons("x", Cons("x", Nil()))))), Stream.toList(Stream.take(Stream.constant("x"))(5)))
 }
